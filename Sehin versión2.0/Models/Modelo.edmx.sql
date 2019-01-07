b04ec2dcf88afd048f205ef1f911a783b312dd58
Domi.Barrozo <Domi.Barrozo@LAPTOP-T0MLI7M5>
2019-01-07 20:56:52 -0300

    estilos

--- Sehin versión2.0/Models/Modelo.edmx.sql
+++ Sehin versión2.0/Models/Modelo.edmx.sql
@@ -2,8 +2,8 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 01/05/2019 13:20:51
--- Generated from EDMX file: C:\Users\marcos\Source\Repos\Sehin-versi-n2.0\Sehin versión2.0\Models\Modelo.edmx
+-- Date Created: 01/07/2019 19:54:30
+-- Generated from EDMX file: C:\Users\Domi.Barrozo\Desktop\Sehin\Sehin versión2.0\Sehin versión2.0\Models\Modelo.edmx
 -- --------------------------------------------------
 
 SET QUOTED_IDENTIFIER OFF;
